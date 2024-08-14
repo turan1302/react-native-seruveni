11e405f81be7aa8bbea22954c07c91a710fc01f9
turan1302 <printwordex@gmail.com>
2024-08-14 14:32:37 +0300

    ilk kodumuzu yazıyoruz

---
+++ bolum_4/App.tsx
@@ -0,0 +1,50 @@
+import { Component } from "react";
+import { Text, View } from "react-native";
+
+/*
+export default class App extends Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <View>
+          <Text>MFSoftware Blog</Text>
+        </View>
+        <View>
+          <Text>MFSoftware Blog</Text>
+        </View>
+        <View>
+          <Text>MFSoftware Blog</Text>
+        </View>
+        <View>
+          <Text>MFSoftware Blog</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+*/
+
+/*
+
+const App = ()=>{
+  return (
+    <View>
+      <Text>Fonksiyon</Text>
+    </View>
+  )
+}
+
+export default App;
+
+ */
+
---
+++ bolum_4/Test.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import { Text, View } from "react-native";
+
+export default class Test extends Component{
+  render() {
+    return (
+      <View>
+        <Text>React Native Serüveni Bölüm 4</Text>
+        <View>
+          <Text>MFSoftware Blog</Text>
+        </View>
+      </View>
+    )
+  }
+}
