dcdc471a451cf620950384cc07106a455c6d459f
turan1302 <printwordex@gmail.com>
2024-08-18 13:35:16 +0300

    flex wrap kullanımı

---
+++ bolum_8/App.tsx
@@ -0,0 +1,43 @@
+import { Component } from "react";
+import { View } from "react-native";
+
+export default class App extends Component{
+
+  /*
+      flexWrap : wrap | nowrap | wrap-reverse
+
+      -- dikey
+
+       <View style={{flexWrap : "wrap"}}>
+        <View style={{height : 100,width : 100,backgroundColor : "red"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "green"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "blue"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "black"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "orange"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "pink"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "aqua"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "brown"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "cyan"}}/>
+      </View>
+
+
+      -- yatay
+        <View style={{flexDirection : "row",flexWrap : "wrap-reverse"}}>
+        <View style={{height : 100,width : 100,backgroundColor : "red"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "green"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "blue"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "brown"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "aqua"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "black"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "orange"}}/>
+        <View style={{height : 100,width : 100,backgroundColor : "pink"}}/>
+      </View>
+   */
+  render() {
+    return (
+    <View>
+
+    </View>
+    )
+  }
+}
