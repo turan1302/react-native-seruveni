2aa2ed612d1b2e161a2e9523e67faf5c26043ab5
turan1302 <printwordex@gmail.com>
2024-09-05 14:08:04 +0300

    safeareaview kullanımı gerceklestirildi

---
+++ bolum_20/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { SafeAreaView, StatusBar, Text, View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={{flex : 1}}>
+        <Text>sdsa</Text>
+      </SafeAreaView>
+    )
+  }
+}
